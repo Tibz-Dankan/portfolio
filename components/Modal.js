574eaa1a217727324ef8e0e401c7c404ccddec8d
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-10-21 23:41:56 +0300

    fix: apply a unique id for each modal component

--- components/Modal.js
+++ components/Modal.js
@@ -3,22 +3,27 @@ import { html, useState, useEffect, reactive, getRef } from "z-js-framework";
 export const Modal = (props) => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const genModalRef = () => {
+    return `modalRef_${Date.now()}`;
+  };
+  const modalRef = genModalRef();
+
   const openModalHandler = () => {
-    const modal = getRef("modalRef");
+    const modal = getRef(modalRef);
     if (!modal) return;
     setIsOpen(true);
     modal.showModal();
   };
 
   const closeModalHandler = () => {
-    const modal = getRef("modalRef");
+    const modal = getRef(modalRef);
     if (!modal) return;
     setIsOpen(false);
     modal.close();
   };
 
   const closeModalOnClickBackDrop = () => {
-    const modal = getRef("modalRef");
+    const modal = getRef(modalRef);
     if (!modal) return;
     modal.addEventListener("click", function (event) {
       if (event.target === modal) {
@@ -31,18 +36,17 @@ export const Modal = (props) => {
     <div>
       <div onClick="${openModalHandler}">${props.openModalElement}</div>
       <dialog
-        ref="modalRef"
+        ref="${modalRef}"
         class="w-[100vw] h-[100vh] bg-[rgba(0,0,0,0.0)] transition-all ml-4 mt-4
         backdrop:bg-[rgba(0,0,0,0.5)] outline-none"
       >
         <div
           class="w-full h-full flex items-center justify-center  bg-[rgba(0,0,0,0.0)]
           z-10"
-          onClick="${closeModalHandler}"
         >
           <div
-            class="w-auto h-auto p-4 relative shadow-[0px_10px_20px_rgba(0,0,0,0.25)]
-            z-50"
+            class="w-auto h-auto p-4 sm:p-8 relative shadow-[0px_10px_20px_rgba(0,0,0,0.25)]
+            z-50 bg-[#212529] rounded-lg"
           >
             <span onClick="${closeModalHandler}" class="absolute top-2 right-2">
               <img
