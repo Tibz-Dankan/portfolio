3f1fe4ec582ebd34d1ad428e0712f473e5fe1a1b
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-11-06 16:47:13 +0300

    CI/CD: add auto deployment of server to render

--- client/constants/index.js
+++ client/constants/index.js
@@ -4,6 +4,6 @@ let serverURL;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   serverURL = "http://localhost:5000/api/v1";
 } else {
-  serverURL = "https://dankan.onrender.com/api/v1";
+  serverURL = "https://portifolio-3o25.onrender.com/api/v1";
 }
 export { serverURL };
