7b1d4c21048fdd4f5000ddc301c5045c19bfa361
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-11-07 22:21:33 +0300

    fix: update with video content placeholder

--- client/components/Video.js
+++ client/components/Video.js
@@ -1,9 +1,10 @@
 import { html } from "z-js-framework";
 
-export const Video = () => {
+export const Video = (props) => {
   return html`<div
-    class="w-96 h-72 flex items-center text-gray-100 bg-purple-500"
+    class="w-96 h-72 flex items-center justify-center text-gray-100 gap-2"
   >
-    Video content for each project here
+    <span> ${props.name} </span>
+    <span> Video Here </span>
   </div>`;
 };
